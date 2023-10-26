5c6eafca10f9d17b00bc769be610e9a4ab3e8c74
Angelo Marasa <angelo.marasa@kaleidico.com>
2023-10-26 00:40:48 -0400

    Added PUC

---
+++ branding-settings.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * Plugin Name: Branding Settings
+ * Description: A plugin to manage branding settings.
+ * Version: 1.1
+ * Author: Angelo Marasa
+ */
+
+/* -------------------------------------------------------------------------------------- */
+// Updated
+require 'puc/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+    'https://github.com/amarasa/branding-settings',
+    __FILE__,
+    'brand-settings-plugin'
+);
+
+//Set the branch that contains the stable release.
+//$myUpdateChecker->setBranch('stable-branch-name');
+
+//Optional: If you're using a private repository, specify the access token like this:
+// $myUpdateChecker->setAuthentication('your-token-here');
+
+/* -------------------------------------------------------------------------------------- */
+
+// Include the shortcode file
+include(plugin_dir_path(__FILE__) . 'shortcode.php');
+
+// Hook to add admin menu
+add_action('admin_menu', 'branding_settings_menu');
+
+// Action function to add admin menu and settings page
+function branding_settings_menu()
+{
+    add_menu_page(
+        'Branding Settings',
+        'Branding Settings',
+        'manage_options',
+        'branding_settings',
+        'branding_settings_page'
+    );
+}
+
+// Function to display the settings page content
+function branding_settings_page()
+{
+?>
+    <div class="wrap">
+        <h2>Branding Settings</h2>
+        <?php
+        acf_form([
+            'id' => 'branding-settings-form',
+            'post_id' => 'options',
+            'new_post' => false,
+            'submit_value' => 'Update Settings'
+        ]);
+        ?>
+        <hr>
+        <h3>Available Shortcodes</h3>
+        <div id="available-shortcodes">
+            <?php
+            if (have_rows('field_value_pairs', 'option')) :
+                echo '<ul>';
+                while (have_rows('field_value_pairs', 'option')) : the_row();
+                    $field = get_sub_field('field');
+                    echo '<li>[branding key="' . esc_html($field) . '"]</li>';
+                endwhile;
+                echo '</ul>';
+            else :
+                echo '<p>No shortcodes available.</p>';
+            endif;
+            ?>
+        </div>
+    </div>
+<?php
+}
+
+if (function_exists('acf_add_local_field_group')) :
+
+    acf_add_local_field_group(array(
+        'key' => 'group_branding_settings',
+        'title' => 'Branding Settings',
+        'fields' => array(
+            array(
+                'key' => 'field_branding_pairs',
+                'label' => 'Field-Value Pairs',
+                'name' => 'field_value_pairs',
+                'type' => 'repeater',
+                'sub_fields' => array(
+                    array(
+                        'key' => 'field_branding_pair_field',
+                        'label' => 'Field',
+                        'name' => 'field',
+                        'type' => 'text',
+                    ),
+                    array(
+                        'key' => 'field_branding_pair_value',
+                        'label' => 'Value',
+                        'name' => 'value',
+                        'type' => 'text',
+                    ),
+                    array(
+                        'key' => 'field_branding_pair_type',
+                        'label' => 'Type',
+                        'name' => 'type',
+                        'type' => 'select',
+                        'choices' => array(
+                            'standard_text' => 'Standard Text',
+                            'email' => 'Email',
+                            'phone' => 'Phone',
+                        ),
+                        'default_value' => 'standard_text',
+                    ),
+                ),
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'branding_settings',
+                ),
+            ),
+        ),
+    ));
+
+endif;
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Brand Settings ===
+Contributors: Angelo Marasa
+Requires at least: 4.7
+Tested up to: 5.4
+Stable tag: 4.3
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+A plugin that allowed you to create brand settings in custom fields and then display through a shortcode.
+
+== Description ==
+
+This plugin allows you to configure branding details in the settings and then use a shortcode anywhere on the site to display the values. This makes it easy to make changes sitewide, without having to change branding in multiple areas.
+== Frequently Asked Questions ==
+
+= Does this require any 3rd party plugins? =
+
+Yes, you will need to have Advanced Custom Fields installed in order for this plugin to work.
+
+
+== Changelog ==
+
+= 1.1 = 
+* Added ability to update through GitHub
+
+= 1.0 =
+* First production release
